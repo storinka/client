52f2d9dca95365bd2e549a137e297581c8d9bdfa
Davyd Kohut <expexes@gmail.com>
2020-12-07 04:06:52 +0200

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
 import StorinkaClient from "./StorinkaClient";
 import CoreError from "./CoreError";
 
-const storinka = new StorinkaClient({
-    clientId: 123,
-    clientSecret: "supersecret",
-});
-
-storinka.invoke("opendata.getCafe", {
-    id: "demo",
-}).then(cafe => {
-    console.log(cafe);
-})
-
 export {
     CoreError
 };
